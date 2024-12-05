4aef74dcbd18cdf563833afc93d22b0e783fe987
Davidson Schaly <davidson.schaly@gmail.com>
2024-12-04 22:55:10 -0300

    feat - Header, Banner, Footer, and RootLayout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,15 +4,16 @@ import cartIcon from "../../images/cartIcon.png";
 import { BiCaretDown } from "react-icons/bi";
 import { HiOutlineSearch } from "react-icons/hi";
 import { SlLocationPin } from "react-icons/sl";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="w-full h-20 bg-amazon_blue text-lightText sticky top-0 z-50">
       <div className="h-full w-full mx-auto inline-flex items-center justify-between gap-1 mdl:gap-3 px-4">
         {/* Logo */}
-        <div className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 flex items-center justify-between h-[70%]">
+        <Link href="/" className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 flex items-center justify-between h-[70%]">
           <Image src={logo} alt="logoImg" className="w-28 object-cover mt-1" />
-        </div>
+        </Link>
 
         {/* Delivery */}
         <div className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 flex items-center justify-between h-[70%] hidden: xl:inline-flex gap-1">
@@ -53,7 +54,7 @@ const Header = () => {
         </div>
 
         {/* Cart */}
-        <div className="flex items-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%] relative">
+        <Link href="/cart" className="flex items-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%] relative">
           <Image
             className="w-auto object-cover h-8"
             src={cartIcon}
@@ -61,7 +62,7 @@ const Header = () => {
           />
           <p className="text-xs text-white font-bold mt-3">Cart</p>
           <span className="absolute text-amazon_yellow text-sm top-2 left-[29px] font-semibold">0</span>
-        </div>
+        </Link>
       </div>
     </div>
   );
