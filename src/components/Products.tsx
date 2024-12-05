e1997c67f5e8fe3c6143f3f4246c99635430a9bd
Davidson Schaly <davidson.schaly@gmail.com>
2024-12-05 01:28:21 -0300

    feat - redux-persist; cart and favorites handling

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -3,8 +3,12 @@ import { ProductProps } from "../../type";
 import { HiShoppingCart } from "react-icons/hi";
 import { FaHeart } from "react-icons/fa";
 import FormattedPrice from "./FormattedPrice";
+import { useDispatch } from "react-redux";
+import { addToCart, addToFavorite } from "@/store/nextSlice";
 
 const Products = ({ productData }: any) => {
+  const dispatch = useDispatch();
+
   return (
     <div className="w-full px-6 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
       {productData.map(
@@ -32,10 +36,46 @@ const Products = ({ productData }: any) => {
                 alt="productImage"
               />
               <div className="w-12 h-24 absolute bottom-10 right-0 border-[1px] border-gray-400 bg-white rounded-md flex flex-col translate-x-20 group-hover:translate-x-0 transition-transform duration-300">
-                <span className="w-full h-full border-b-[1px] border-b-gray-400 flex items-center justify-center text-xl bg-transparent hover:bg-amazon_yellow cursor-pointer duration-300">
+                <span
+                  className="w-full h-full border-b-[1px] border-b-gray-400 flex items-center justify-center text-xl bg-transparent hover:bg-amazon_yellow cursor-pointer duration-300"
+                  onClick={() =>
+                    dispatch(
+                      addToCart({
+                        _id,
+                        brand,
+                        category,
+                        image,
+                        description,
+                        isNew,
+                        oldPrice,
+                        price,
+                        title,
+                        quantity: 1,
+                      })
+                    )
+                  }
+                >
                   <HiShoppingCart />
                 </span>
-                <span className="w-full h-full border-b-[1px] border-b-gray-400 flex items-center justify-center text-xl bg-transparent hover:bg-amazon_yellow cursor-pointer duration-300">
+                <span
+                  className="w-full h-full border-b-[1px] border-b-gray-400 flex items-center justify-center text-xl bg-transparent hover:bg-amazon_yellow cursor-pointer duration-300"
+                  onClick={() =>
+                    dispatch(
+                      addToFavorite({
+                        _id,
+                        brand,
+                        category,
+                        image,
+                        description,
+                        isNew,
+                        oldPrice,
+                        price,
+                        title,
+                        quantity: 1,
+                      })
+                    )
+                  }
+                >
                   <FaHeart />
                 </span>
               </div>
@@ -60,7 +100,27 @@ const Products = ({ productData }: any) => {
               <p className="text-xs text-gray-600 text-justify">
                 {description.substring(0, 120)}
               </p>
-              <button className="h-10 font-medium bg-amazon_blue text-white rounded-md hover:bg-amazon_yellow hover:text-black duration-300 mt-2">Add to cart</button>
+              <button
+                onClick={() =>
+                  dispatch(
+                    addToCart({
+                      _id,
+                      brand,
+                      category,
+                      image,
+                      description,
+                      isNew,
+                      oldPrice,
+                      price,
+                      title,
+                      quantity: 1,
+                    })
+                  )
+                }
+                className="h-10 font-medium bg-amazon_blue text-white rounded-md hover:bg-amazon_yellow hover:text-black duration-300 mt-2"
+              >
+                Add to cart
+              </button>
             </div>
           </div>
         )
--- type.d.ts
+++ type.d.ts
@@ -9,3 +9,23 @@ export interface ProductProps {
   price: number;
   title: string;
 }
+
+export interface StoreProduct {
+  _id: number;
+  brand: string;
+  category: string;
+  description: string;
+  image: string;
+  isNew: boolean;
+  oldPrice: number;
+  price: number;
+  title: string;
+  quantity: number;
+}
+
+export interface StateProps {
+  productData: [];
+  favoriteData: [];
+  userInfo: null | string;
+  next: any;
+}
