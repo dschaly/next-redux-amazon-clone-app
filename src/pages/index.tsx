d8c12f505b18a7c3a73f577566f56984d2913d09
Davidson Schaly <davidson.schaly@gmail.com>
2024-12-04 21:38:38 -0300

    feat - BottomHeader component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Header from '@/components/header/Header'
+import BottomHeader from '@/components/header/BottomHeader'
 
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <BottomHeader />
     </main>
   )
 }
